1a6f21a494cb2f8be8407396b7f5fb8025f190c9
Yunzhi Gao <yunzhi.gao@airbnb.com>
2016-01-16 12:12:10 -0800

    commit

---
+++ dushu_1601/client/scripts/routes.js
@@ -0,0 +1,49 @@
+angular
+  .module('Whatsapp')
+  .config(config);
+ 
+function config($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'client/templates/tabs.html',
+      resolve: {
+        // user() {
+        //   if (!Meteor.user()) {
+        //     throw 'AUTH_REQUIRED';
+        //   }
+        // },
+      }
+    })
+    .state('tab.chat', {
+      url: '/chat',
+      views: {
+        'tab-chat': {
+          templateUrl: 'client/templates/chat.html',
+          controller: 'ChatCtrl as chat'
+        }
+      }
+    })
+    .state('tab.profile', {
+      url: '/profile',
+      views: {
+        'tab-profile': {
+          templateUrl: 'client/templates/profile.html',
+          controller: 'ProfileCtrl as ProfileCtrl',
+        }
+      }
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'client/templates/login.html',
+      controller: 'LoginCtrl as logger'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'client/templates/register.html',
+      controller: 'RegisterCtrl as register'
+    });
+ 
+  $urlRouterProvider.otherwise('tab/chat');
+}
